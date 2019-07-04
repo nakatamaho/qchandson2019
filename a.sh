5fad23927b39010b7c96f8d5adbdd966e0dd0be9
NAKATA Maho <maho.nakata@gmail.com>
2019-07-04 15:43:57 +0900

    update

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-mv 02_h2_sto3g_hf_overlap.dat	 03_h2_sto3g_hf_overlap.dat	 
-mv 03_h2_sto3g_hf_kinetic.dat	 04_h2_sto3g_hf_kinetic.dat	 
-mv 04_h2_sto3g_hf_potential.dat	 05_h2_sto3g_hf_potential.dat	 
-mv 05_h2_sto3g_hf_eriao.dat      06_h2_sto3g_hf_eriao.dat      
+mv 02_h2_sto3g_hf_overlap.dat	 03_h2_sto3g_hf_overlap.dat	
+mv 03_h2_sto3g_hf_kinetic.dat	 04_h2_sto3g_hf_kinetic.dat	
+mv 04_h2_sto3g_hf_potential.dat	 05_h2_sto3g_hf_potential.dat	
+mv 05_h2_sto3g_hf_eriao.dat      06_h2_sto3g_hf_eriao.dat     
